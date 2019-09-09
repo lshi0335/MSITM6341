cac3f08e6bfb01680adc272a26ec8c0aeaf1e4a8
lshi0335 <lshi0335@users.noreply.github.com>
2019-09-08 21:20:58 -0500

    Changed comments on variable price

--- Assignments/homework_assignment_2/working_with_variables.py
+++ Assignments/homework_assignment_2/working_with_variables.py
@@ -5,7 +5,7 @@
 
 # Assignment 2
 stock_symbol = "DIS" # The tick symbol for The Walt Disney Company
-price = 139.55 # Closing price on 9/5/2019
+price = 139.55 # Closing price on 9/6/2019
 price_change = 0.71 # Change in closing price from 9/5/2019 to 9/6/2019
 
 print("Symbol: " + stock_symbol.upper() + ", Price: " + str(price) + ", Change: " + str(price_change) + "\n\n")
