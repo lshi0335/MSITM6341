3437b42cf155ae9baa9a858535f30685912ee07e
lshi0335 <lshi0335@users.noreply.github.com>
2019-09-22 22:37:34 -0500

    Homework 4

---
+++ Assignments/homework_assignment_4/working_with_lists_2.py
@@ -0,0 +1,14 @@
+# Lei Shi
+# 0985491
+# 9/22/2019
+# MSITM 6341
+
+# Assignment 4
+
+# Create a list of odd numbers between 3 and 51
+odd_number = [2*x+1 for x in range(1,26)]
+
+# Print first 5 elements in the odd number list
+for num in range(0,5):
+    print(odd_number[num])
+
