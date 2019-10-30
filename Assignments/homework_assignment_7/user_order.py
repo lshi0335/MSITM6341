f7eee83cb39314a3aee9cf915be663696a1c5c21
lshi0335 <lshi0335@users.noreply.github.com>
2019-10-29 20:49:28 -0500

    Submitting homework 7

--- Assignments/homework_assignment_7/user_order.py
+++ Assignments/homework_assignment_7/user_order.py
@@ -4,3 +4,40 @@
 #MSITM 6341
 
 
+# Define menu dictionary
+menu_dictionary = {
+    'Tacos': 5.9,
+    'Chips': 2.9,
+    'Salsa': 3.4,
+    'Quesadilla': 6.9,
+    'Burrito': 6.9,
+    'Fajita': 10.0
+    }
+
+# Initialize customer order
+customer_order = []
+#prompt = "\nPlease enter item to order: "
+#prompt += "\nPlease entern 'N' to finish your order. "
+
+order_active = True
+
+while order_active:
+    # Prompt for order item
+    item = input('Enter an item: ')
+    if item == 'N':
+        break
+    else:
+        customer_order.append(item)
+
+total_price = 0 # Inital order total is 0
+for item in customer_order:
+   if item in menu_dictionary.keys():
+       print(item.title() + ': ' + '${:,.1f}'.format(menu_dictionary[item]))
+       total_price = total_price + menu_dictionary[item] #Cummulatively add to order total if order item is in menu
+   else:
+       print('We do not have ' + item.title() + '.')
+print('---------------\n Order Total: '+ '${:,.1f}'.format(total_price))
+
+
+
+
