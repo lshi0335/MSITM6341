cee1abb655f6d778209044ce16ed097c3862f458
lshi0335 <lshi0335@users.noreply.github.com>
2019-11-05 21:16:35 -0600

    11/05/2019 Attendance question updated

--- Attendance/check_string_numbers.py
+++ Attendance/check_string_numbers.py
@@ -1,10 +1,17 @@
-a = '10'
+a = '101'
 b = '11'
 
 def comp_str(x='', y = ''):
     x_num = float(x)
     y_num = float(y)
-    print('x = y:')
-    print(x_num == y_num)
+    print('x=' + x + '; ' + 'y=' + y)
+    if x_num == y_num:
+        print('x = y')
+    elif x_num > y_num:
+        print('x > y')
+    else:
+        print('x < y')
 
 comp_str(a,b)
+comp_str('2.236','3.1415926535')
+comp_str(a,'101')
