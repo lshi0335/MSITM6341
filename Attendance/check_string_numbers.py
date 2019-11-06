520ef9f6303dd5de219b8d899e65a6ac95a9fbb4
lshi0335 <lshi0335@users.noreply.github.com>
2019-11-05 21:27:15 -0600

    11/05/2019 Attendance question: added error msg.

--- Attendance/check_string_numbers.py
+++ Attendance/check_string_numbers.py
@@ -2,16 +2,21 @@ a = '101'
 b = '11'
 
 def comp_str(x='', y = ''):
-    x_num = float(x)
-    y_num = float(y)
-    print('x=' + x + '; ' + 'y=' + y)
-    if x_num == y_num:
-        print('x = y')
-    elif x_num > y_num:
-        print('x > y')
-    else:
-        print('x < y')
+    print('\nx=' + x + '; ' + 'y=' + y)
+    try:       
+        x_num = float(x)
+        y_num = float(y)
+        if x_num == y_num:
+            print('x = y')
+        elif x_num > y_num:
+            print('x > y')
+        else:
+            print('x < y')
+    except ValueError:
+        return print('Error: Function comp_str() requires two string (or numeric) inputs.'
+                    +'\nPlease make sure that the string input has valid numeric meaning.')
 
 comp_str(a,b)
 comp_str('2.236','3.1415926535')
 comp_str(a,'101')
+comp_str('2.236','pi')
